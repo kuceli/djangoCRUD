2fe7fac7e842b58b660b0eecbdc49b1416b6f3f3
Englama Kuceli Susan <kucelienglama@ymail.com>
2022-10-28 20:28:42 +0100

    first commit

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from musicapp.models import *
+
+class ArtisteAdmin(admin.ModelAdmin):
+    list_display = ["id", "first_name", "last_name", "age"]
+
+class SongAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "date_released", "likes"]
+
+class LyricAdmin(admin.ModelAdmin):
+    list_display = ["id", "song", "content"]
+
 
 # Register your models here.
+
+admin.site.register(Artiste, ArtisteAdmin)
+admin.site.register(Song, SongAdmin)
+admin.site.register(Lyric, LyricAdmin)
+
--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,33 @@
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    age = models.IntegerField()
+
+    class Meta:
+        verbose_name_plural='Artists'
+    
+    def __str__(self): 
+        return f"{self.first_name}   {self.last_name}   {self.age} "
+
+class Song(models.Model):
+    artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    title = models.CharField(max_length=40)
+    date_released = models.DateField(auto_now=False)
+    likes = models.IntegerField()
+    # artiste_id = models.IntegerField()
+
+    def __str__(self): 
+        return f"{self.title}"
+
+class Lyric(models.Model):
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    # song_id = models.IntegerField() 
+
+    def __str__(self): 
+        return f"{self.song.title}   {self.content}"
